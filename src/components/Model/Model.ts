9770b805eaaf38c43216f778962850c731e7c14b
anvyden <anvyden@gmail.com>
2022-05-04 00:38:30 +0700

    added methods of Model: getValue, setValue, increment, decrement

--- src/components/Model/Model.ts
+++ src/components/Model/Model.ts
@@ -1,5 +1,7 @@
 type Orientation = 'vertical' | 'horizontal'
 type Color = 'green'
+type Option = keyof ISettings
+type OptionValue = number | boolean | Orientation | Color
 
 interface ISettings {
   min: number,
@@ -41,7 +43,27 @@ class Model {
     this.state = { ...newState } // Нужно добавить валидацию параметров слайдера.
   }
 
-  get getState(): ISettings {
+  public getState(): ISettings {
     return this.state
+  }
+
+  public setValue(option: Option, value: OptionValue): void {
+    const newOptionValue: object = { [option]: value }
+    // Нужна валидация
+    this.state = { ...this.state, ...newOptionValue }
+  }
+
+  public getValue(option: Option): OptionValue {
+    return this.state[option]
+  }
+
+  public increment(option: number): void {
+    const newOptionValue: number = this.state[option] + this.state.step
+    // Нужна валидация нового значения, чтоб оно не выходило за границы допустимых
+  }
+
+  public decrement(option: number): void {
+    const newOptionValue: number = this.state[option] - this.state.step
+    // Нужна валидация нового значения, чтоб оно не выходило за границы допустимых
   }
 }
