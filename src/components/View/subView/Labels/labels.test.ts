c4b8734661809cb1df55997643d9595f01edcde9
anvyden <anvyden@gmail.com>
2022-06-05 22:18:24 +0700

    feat: created some tests for labels (coverage about 100%)

---
+++ src/components/View/subView/Labels/labels.test.ts
@@ -0,0 +1,61 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Labels from "./Labels";
+import {ISettings} from "../../../interfaces/interfaces";
+import {defaultState} from "../../../../defaultState";
+
+describe('Labels:', () => {
+  let labels: Labels
+  let root: HTMLElement
+  let state: ISettings
+  let event: PointerEvent
+
+  beforeEach(() => {
+    state = {
+      ...JSON.parse(JSON.stringify(defaultState)),
+      labels: {
+        addLabels: true,
+        countOfLabels: 6
+      }
+    }
+    root = document.createElement('div')
+    event = <PointerEvent>new Event('pointerdown')
+  })
+
+  test('should be instance of Labels', () => {
+    labels = new Labels(state)
+    expect(labels).toBeInstanceOf(Labels)
+  })
+
+  test('should return HTMLDivElement of labels', () => {
+    labels = new Labels(state)
+    expect(labels.getLabels()).toBeTruthy()
+  })
+
+  test('should created labels', () => {
+    labels = new Labels(state)
+    const newLabelsNode = labels.getLabels()
+    root.appendChild(newLabelsNode)
+
+    expect(root.querySelectorAll('.slider__labels').length).toBe(1)
+  })
+
+  test('should emit the event once', () => {
+    labels = new Labels(state)
+    const spyEmit = jest.spyOn(labels, 'emit')
+    const body = document.querySelector('body')
+
+    if (body instanceof HTMLBodyElement) {
+      const labelsNode = labels.getLabels()
+
+      body.appendChild(root)
+      root.appendChild(labelsNode)
+
+      labelsNode.dispatchEvent(event)
+      expect(spyEmit).toHaveBeenCalledTimes(1)
+    }
+  })
+
+})
